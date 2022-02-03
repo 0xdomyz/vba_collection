d390bf27f6f5039e37f91f686cf33045cf815585
0xdomyz <yzdominic@outlook.com>
2022-02-03 19:08:28 +1100

    add ve func, reorg doco

--- copy_paste.vb
+++ copy_paste.vb
@@ -1,96 +1,75 @@
-'Copy paste a block of cells
+'Assign values from range a to range b
 '
-'Copy a continuous row, column, or table of cells.
-'Continuous means no empty cells on top and left edges of the block.
-'Then paste into another location represented by a cell. Top left corner of the 
-'block is to be placed into this representative cell.
+'ve_blk(a,b,pst_mod):
+'Range a represented by top left corner cell of a continuous block.
+'Continuous means no empty cells on top and left edges.
+'Range b has same dimension as range a.
+'
+'ve_wh(a,w,h,b,pst_mod):
+'Ranges defined by top left corner cells and their width and height.
+'
+'ve(a,b,pst_mod):
+'Specify ranges fully.
 '
 'Inputs:
-'   a : Top left cell of the original block.
-'   b : Representative cell of the target location.
-'   pst_mod : Optional Excel paste mode integer. See sub cp for details.
-'       Default : xlPasteValues
-'
-'Effects:
-'   A copy paste action.
+'   a : Top left cell of the original block. Original range for ve.
+'   w : Width of the table.
+'   h : Height of the table.
+'   b : Top left cell of the target location. Target range for ve.
 '
 'Example
 '
-'Copy paste a block of cells from A1 to E1
+'Assign value for a block of cells from A1 to E1
 '   A  B  C
 '1  1  2
 '2  3  4
 '3     5  6
-'Call cp_blk(Range("A1"), Range("E1"))
+'Call ve_blk(Range("A1"), Range("E1"))
+'Call ve_wh(Range("A1"), 2, 2, Range("E1"))
+'Call ve(Range("A1:B2"), Range("E1"))
 '   E  F  G
 '1  1  2
 '2  3  4
 '3
-Sub cp_blk( _
-    ByRef a As Range, _
-    ByRef b As Range, _
-    Optional ByVal pst_mod As Integer = xlPasteValues)
+Sub ve_blk(ByRef a As Range, ByRef b As Range)
 Dim w As Integer
 Dim h As Integer
 w = sf_end(a, "right").Column - a.Column + 1
 h = sf_end(a, "down").Row - a.Row + 1
-Call cp_wh(a,w,h,b,pst_mod)
+Call ve_wh(a,w,h,b)
 End Sub
 
-'Copy paste a range defined by dimension
+Sub ve_wh( _
+    ByRef a As Range, _
+    ByVal w As Integer, _
+    ByVal h As Integer, _
+    ByRef b As Range)
+Call ve(Range(a, a.Offset(h - 1, w - 1)),Range(b, b.Offset(h - 1, w - 1)))
+End Sub
+
+Sub ve(ByRef a As Range, ByRef b As Range)
+b.value = a.value
+End Sub
+
+'Copy paste range a to range b
 '
-'Dimension is the width and height of the table being copied.
-'Then paste into another location represented by a cell. Top left corner of the 
-'original table is to be placed into this representative cell.
+'cp_blk(a,b,pst_mod):
+'Range a represented by top left corner cell of a continuous block.
+'Continuous means no empty cells on top and left edges.
+'
+'cp_wh(a,w,h,b,pst_mod):
+'Range a defined by top left corner cell and it's width and height.
+'
+'cp(a,b,pst_mod):
+'Specify range a fully.
 '
 'Inputs:
-'   a : Top left cell of the original table.
+'   a : Top left cell of the original block. Original range for cp.
 '   w : Width of the table.
 '   h : Height of the table.
-'   b : Representative cell of the target location.
-'   pst_mod : Optional Excel paste mode integer. See sub cp for details.
-'       Default : xlPasteValues
-'
-'Effects:
-'   A copy paste action.
-'
-'Example
-'
-'Copy paste a block of cells from A1 to E1
-'   A  B  C
-'1  1  2
-'2  3  4
-'3     5  6
-'Call cp_wh(Range("A1"), 2, 2, Range("E1"))
-'   E  F  G
-'1  1  2
-'2  3  4
-'3
-Sub cp_wh( _
-    ByRef a As Range, _
-    ByRef w As Integer, _
-    ByRef h As Integer, _
-    ByRef b As Range, _
-    Optional ByVal pst_mod As Integer = xlPasteValues)
-Dim br As Range
-Set br = a.Offset(h - 1, w - 1)
-Call cp(Range(a, br),b,pst_mod)
-End Sub
-
-'Copy paste a range
-'
-'Copy a range.
-'Then paste into another location represented by a cell. Top left corner of the 
-'original range is to be placed into this representative cell.
-'
-'Inputs:
-'   a : Original range.
-'   b : Representative cell of the target location.
+'   b : Top left corner of the target location.
 '   pst_mod : Optional Excel paste mode integer.
 '       Default : xlPasteValues
-'
-'Effects:
-'   A copy paste action.
 '
 'Notes:
 'List of Excel paste modes:
@@ -107,11 +86,33 @@ End Sub
 '1  1  2
 '2  3  4
 '3     5  6
+'Call cp_blk(Range("A1"), Range("E1"))
+'Call cp_wh(Range("A1"), 2, 2, Range("E1"))
 'Call cp(Range("A1:B2"), Range("E1"))
 '   E  F  G
 '1  1  2
 '2  3  4
 '3
+Sub cp_blk( _
+    ByRef a As Range, _
+    ByRef b As Range, _
+    Optional ByVal pst_mod As Integer = xlPasteValues)
+Dim w As Integer
+Dim h As Integer
+w = sf_end(a, "right").Column - a.Column + 1
+h = sf_end(a, "down").Row - a.Row + 1
+Call cp_wh(a,w,h,b,pst_mod)
+End Sub
+
+Sub cp_wh( _
+    ByRef a As Range, _
+    ByVal w As Integer, _
+    ByVal h As Integer, _
+    ByRef b As Range, _
+    Optional ByVal pst_mod As Integer = xlPasteValues)
+Call cp(Range(a, a.Offset(h - 1, w - 1)),b,pst_mod)
+End Sub
+
 Sub cp( _
     ByRef a As Range, _
     ByRef b As Range, _
