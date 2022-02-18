5623ad4e97007f88247238234c1493982a2644dd
0xdomyz <yzdominic@outlook.com>
2022-02-19 01:49:25 +1100

    formating formula codes

--- custom_formula.vb
+++ custom_formula.vb
@@ -1,112 +1,83 @@
+Function Blackscholes(K As Double, t As Double, S As Double, _
+        r As Double, sigma As Double)
+    Dim d1 As Double
+    Dim d2 As Double
 
-Function Blackscholes(K As Double, t As Double, S As Double, r As Double, sigma As Double)
-Dim d1 As Double
-Dim d2 As Double
+    d1 = (WorksheetFunction.Ln(S / K) + (r + sigma ^ 2 / 2) * t) / (sigma * t ^ 0.5)
+    d2 = d1 - sigma * Sqr(t)
 
-d1 = (WorksheetFunction.Ln(S / K) + (r + sigma ^ 2 / 2) * t) / (sigma * t ^ 0.5)
-d2 = d1 - sigma * Sqr(t)
-
-Blackscholes = S * WorksheetFunction.NormSDist(d1) - K * Exp(-r * t) * WorksheetFunction.NormSDist(d2)
+    Blackscholes = S * WorksheetFunction.NormSDist(d1) - _
+        K * Exp(-r * t) * WorksheetFunction.NormSDist(d2)
 End Function
 
 
-Function americanput(n As Long, K As Double, t As Double, S As Double, r As Double, sigma As Double)
+Function americanput(n As Long, K As Double, t As Double, S As Double, _
+        r As Double, sigma As Double)
+    Dim u As Double
+    Dim d As Double
+    Dim deltaT As Double
+    Dim j As Integer
+    Dim putprice() As Double
+    Dim stockprice() As Double
+    Dim q As Double
+    ReDim putprice(n, n)
+    ReDim stockprice(n, n)
 
-Dim u As Double
-Dim d As Double
-Dim deltaT As Double
-Dim j As Integer
-Dim putprice() As Double
-Dim stockprice() As Double
-Dim q As Double
+    deltaT = t / n
+    u = Exp(sigma * Sqr(deltaT))
+    d = Exp(-sigma * Sqr(deltaT))
+    q = (Exp(r * deltaT) - d) / (u - d)
 
-ReDim putprice(n, n)
-ReDim stockprice(n, n)
+    For j = 0 To n
+        putprice(j, n) = WorksheetFunction.Max(K - S * u ^ (n - j) * d ^ j, 0)
+    Next j
 
+    For j = 0 To n
+        For i = 0 To j
+            stockprice(i, j) = S * u ^ (j - i) * d ^ i
+        Next i
+    Next j
 
-deltaT = t / n
+    For j = n - 1 To 0 Step - 1
+        For i = 0 To j
+        putprice(i, j) = WorksheetFunction.Max( _
+            Exp(-r * deltaT) * (q * putprice(i, j + 1) + (1 - q) * _
+                putprice(i + 1, j + 1)), _
+            K - stockprice(i, j) _
+        )
+        Next i
+    Next j
 
-u = Exp(sigma * Sqr(deltaT))
-d = Exp(-sigma * Sqr(deltaT))
-
-q = (Exp(r * deltaT) - d) / (u - d)
-
-
-
-For j = 0 To n                'payoff at END
-
-putprice(j, n) = WorksheetFunction.Max(K - S * u ^ (n - j) * d ^ j, 0) 'put prices at time N
-                          
-Next j
-
-
-
-For j = 0 To n              'stock price at (i,j)
-
-    For i = 0 To j
-    
-        stockprice(i, j) = S * u ^ (j - i) * d ^ i
-                
-    Next i
-
-Next j
-
-
-
-
-For j = n - 1 To 0 Step -1          'payoff's at (i,j)
-
-    For i = 0 To j
-    
-    putprice(i, j) = WorksheetFunction.Max(Exp(-r * deltaT) * (q * putprice(i, j + 1) + (1 - q) * putprice(i + 1, j + 1)), K - stockprice(i, j))
-    
-    Next i
-
-Next j
-
-
-    
-americanput = putprice(0, 0)
-
-
+    americanput = putprice(0, 0)
 End Function
 
 
-Function european(n As Long, K As Double, t As Double, S As Double, r As Double, sigma As Double)
+Function european(n As Long, K As Double, t As Double, S As Double, _
+        r As Double, sigma As Double)
+    Dim u As Double
+    Dim d As Double
+    Dim deltaT As Double
+    Dim j As Integer
+    Dim q As Double
+    Dim pay As Double
+    Dim cal As Double
+    Dim payoff() As Double
+    Dim callprice() As Double
+    ReDim payoff(n)
+    ReDim callprice(n)
 
+    deltaT = t / n
+    u = Exp(sigma * Sqr(deltaT))
+    d = Exp(-sigma * Sqr(deltaT))
+    q = (Exp(r * deltaT) - d) / (u - d)
 
-Dim u As Double
-Dim d As Double
-Dim deltaT As Double
-Dim j As Integer
-Dim q As Double
-Dim pay As Double
-Dim cal As Double
-Dim payoff() As Double
-Dim callprice() As Double
+    For j = 0 To n
+        payoff(j) = WorksheetFunction.Max(S * u ^ (n - j) * d ^ j - K, 0)
+        callprice(j) = Exp(-r * t) * WorksheetFunction.Combin(n, j) * _
+            q ^ (n - j) * (1 - q) ^ j * payoff(j)
+    Next j
 
-ReDim payoff(n)
-ReDim callprice(n)
-
-
-deltaT = t / n
-
-u = Exp(sigma * Sqr(deltaT))
-d = Exp(-sigma * Sqr(deltaT))
-
-q = (Exp(r * deltaT) - d) / (u - d)
-
-
-For j = 0 To n
-
-    payoff(j) = WorksheetFunction.Max(S * u ^ (n - j) * d ^ j - K, 0)
-
-    callprice(j) = Exp(-r * t) * WorksheetFunction.Combin(n, j) * q ^ (n - j) * (1 - q) ^ j * payoff(j)
-
-Next j
-
-european = WorksheetFunction.Sum(callprice)
-
+    european = WorksheetFunction.Sum(callprice)
 End Function
 
 
